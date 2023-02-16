1edb66714c203a56ed65e62691cfc8e943fdb986
Evandrodev33 <evandro.dev22@gmail.com>
2023-02-16 20:38:16 -0300

    modificação-01

--- Desafios Javascript/desafio2nomes.js
+++ Desafios Javascript/desafio2nomes.js
@@ -1,6 +1,6 @@
-const firstName = 'Evandro'
-const seccondName = ' Reis'
+ const firstName = 'Evandro'
+ const seccondName = 'Joao '
 
-const names = 'Olá, meu nome é ${firstName} e  o do meu partner do devclub é ${seccondName}'
+ const names = `Olá, meu nome é ${firstName} e o do meu partner do devclub  é o ${seccondName}`
 
-console.log(names)
+ console.log (names)
